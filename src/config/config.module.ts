0bc7a243dae58bd88a4b15f78b32d8bde58f29c1
devellybutton <leegr0106@gmail.com>
2025-04-24 00:49:51 +0900

    implement configuration module with Joi validation

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
+import {
+  ConfigModule as NestJSConfigModule,
+  ConfigService,
+} from '@nestjs/config';
+import configuration from './configuration';
 
-@Module({})
+@Module({
+  imports: [
+    NestJSConfigModule.forRoot({
+      load: [configuration],
+      validationSchema: configuration,
+      validationOptions: {
+        abortEarly: false,
+      },
+    }),
+  ],
+  providers: [ConfigService],
+})
 export class ConfigModule {}
---
+++ src/config/configuration.ts
@@ -0,0 +1,21 @@
+import { IConfig } from './config.interface';
+import * as Joi from '@hapi/joi';
+
+export default (): Joi.ObjectSchema<IConfig> =>
+  Joi.object<IConfig>({
+    BACKEND_PORT: Joi.number().required(),
+    POSTGRES_HOST: Joi.string().required(),
+    POSTGRES_PORT: Joi.number().required(),
+    POSTGRES_USER: Joi.string().required(),
+    POSTGRES_PASSWORD: Joi.string().required(),
+    POSTGRES_DB: Joi.string().required(),
+
+    ACCESS_TOKEN_SECRET: Joi.string().required(),
+    ACCESS_TOKEN_EXPIRATION_TIME: Joi.string().required(),
+    REFRESH_TOKEN_SECRET: Joi.string().required(),
+    REFRESH_TOKEN_EXPIRATION_TIME: Joi.string().required(),
+
+    EMAIL_SERVICE: Joi.string().required(),
+    EMAIL_USER: Joi.string().required(),
+    EMAIL_PASSWORD: Joi.string().required(),
+  });
