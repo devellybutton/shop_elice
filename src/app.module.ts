ba7675bd145d206a14c23e1ffb33aa1eb11a225a
devellybutton <leegr0106@gmail.com>
2025-04-23 22:34:18 +0900

    Resolve module name inconsistencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './user/users.module';
-import { ProductsModule } from './product/products.module';
-import { CategoriesModule } from './categories/categories.module';
 import { AuthModule } from './auth/auth.module';
 import { EmailModule } from './email/email.module';
 import { DatabaseModule } from './database/database.module';
@@ -11,9 +8,21 @@ import { RedisModule } from './redis/redis.module';
 import { ConfigModule } from './config/config.module';
 import { CommentModule } from './comment/comment.module';
 import { CommonModule } from './common/common.module';
+import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule, CategoriesModule, AuthModule, EmailModule, DatabaseModule, RedisModule, ConfigModule, CommentModule, CommonModule],
+  imports: [
+    UserModule,
+    ProductModule,
+    AuthModule,
+    EmailModule,
+    DatabaseModule,
+    RedisModule,
+    ConfigModule,
+    CommentModule,
+    CommonModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
